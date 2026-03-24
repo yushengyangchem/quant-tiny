e933ed50ce598a67928a35c3b86f4859124a6f66
yushengyangchem <yushengyangchem@gmail.com>
2026-03-24 17:05:49 +0800

    chore(release): bump version to v0.2.0

--- src/quant_tiny/__init__.py
+++ src/quant_tiny/__init__.py
@@ -2,7 +2,7 @@ from .backtest import Backtest
 from .core import Bar, Order, Portfolio, Position, Side, Strategy
 from .indicators import ema, rsi, sma
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __homepage__ = "https://github.com/yushengyangchem/quant-tiny"
 
 __all__ = [
